e5c9379cc25b3d4bff83491f099f8a2714d89f97
Slava <slavkostrov@gmail.com>
2022-10-29 22:32:01 +0300

    [UPDATE] Updated code w/ Pyspark ML Pipeline

--- dags/tasks/train_model.py
+++ dags/tasks/train_model.py
@@ -4,18 +4,19 @@ Module with tasks for model training
 import datetime
 import sys
 
+from pyspark.ml import Pipeline
+
 _folder = __file__[: __file__.rfind("/") + 1]
 sys.path.extend([_folder, _folder[:_folder.rfind("/") + 1]])
 
 from utils import get_spark
 from base_config import Config
 
-import os
-
 import mlflow
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import Imputer
 import pyspark.sql.functions as F
 import logging
 
@@ -46,22 +47,25 @@ def train_model(config: Config):
 
     # set exp name
     mlflow.set_experiment("flight_delay_model")
-    with mlflow.start_run(run_name=model_name, description="flight_delay_model_evaluation") as active_run:
+    with mlflow.start_run(description="flight_delay_model_evaluation") as active_run:
         features = spark.read.parquet(f"{config.output_prefix}/features_{config.dataset_name}.parquet")
         for day in range(1, 8):
             features = features.withColumn(f"flight_weekday_{day}", (F.dayofweek("FL_DATE") == F.lit(day)).cast("int"))
 
-        assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
-
-        flights_sdf_v2 = assembler.transform(features.dropna())
-        flights_sdf_v2 = flights_sdf_v2.select(['features', 'ARR_DELAY'])
+        outpul_columns = ["{}_imputed".format(c) for c in features.columns]
+        imputer = Imputer(
+            inputCols=features.columns,
+            outputCols=outpul_columns,
+        ).setStrategy("mean")
+        assembler = VectorAssembler(inputCols=outpul_columns, outputCol="features")
+        lr = LinearRegression(featuresCol="features", labelCol="ARR_DELAY")
+        model = Pipeline(stages=[imputer, assembler, lr])
 
         # Splitting into train and test datasets
-        train, test = flights_sdf_v2.randomSplit([0.8, 0.2])
+        train, test = features.randomSplit([0.8, 0.2])
+        model = model.fit(train)
 
-        # Fitting Linear Regression Model
-        lreg_v1 = LinearRegression(featuresCol="features", labelCol="ARR_DELAY")
-        lr_model_v1 = lreg_v1.fit(train)
+        lr_model_v1 = model
 
         # Predicting and Finding R2 and RMSE Values
         predictions_v1 = lr_model_v1.transform(test)
@@ -71,15 +75,8 @@ def train_model(config: Config):
         logger.info("R Squared (R2) on test data = %g" % eval_reg.evaluate(predictions_v1))
         logger.info("Root Mean Squared Error (RMSE) on test data = %g" % test_result_v1.rootMeanSquaredError)
 
-        mlflow.spark.log_model(lr_model_v1, "SparkML-linear-regression")
+        mlflow.spark.log_model(lr_model_v1, "SparkML-linear-regression", registered_model_name=model_name)
         return
-        os.system("hdfs dfs -rm -r tmp_model")
-        fitted_model.save("tmp_model")
-        model_ex = LogisticRegressionModel.load("tmp_model")
-        # temp model saving to local
-        mlflow.spark.save_model(model_ex, "spark-model")
-        # save model to object-storage as artifact
-        mlflow.log_artifacts("spark-model")
 
 
 def eval_model(config: Config):
