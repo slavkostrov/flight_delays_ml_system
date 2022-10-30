cedb2396d7dd5dd92e3236c3fa01da63995c65a3
Slava <slavkostrov@gmail.com>
2022-10-30 15:46:03 +0300

    [FIX] Fixed SQL script

--- dags/features_monitoring.py
+++ dags/features_monitoring.py
@@ -10,30 +10,27 @@ import datetime
 
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator as EmptyOperator
-from airflow.operators.python import PythonOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from base_config import Config
-from tasks.prepare_features import collect_raw_data, clean_data, concat_features
 
 config = Config()
 
 with DAG(
-    f"{config.dag_prefix}features_monitoring",
-    default_args={
-        "depends_on_past": False,
-        "email": [config.user_email],
-        "email_on_failure": True,
-        "email_on_retry": True,
-        "retries": 3,
-    },
-    description="DAG for feature monitoring.",
-    doc_md=__doc__,
-    schedule_interval=None,
-    start_date=datetime.datetime(2022, 10, 30, 10),
-    catchup=False,
-    tags=["critical"],
+        f"{config.dag_prefix}features_monitoring",
+        default_args={
+            "depends_on_past": False,
+            "email": [config.user_email],
+            "email_on_failure": True,
+            "email_on_retry": True,
+            "retries": 3,
+        },
+        description="DAG for feature monitoring.",
+        doc_md=__doc__,
+        schedule_interval=None,
+        start_date=datetime.datetime(2022, 10, 30, 10),
+        catchup=False,
+        tags=["critical"],
 ) as dag:
     dag.doc_md = __doc__
 
@@ -54,7 +51,7 @@ with DAG(
 
     create_feature_stat_table = PostgresOperator(
         task_id="create_feature_stat_table",
-        sql="""
+        sql=f"""
                 CREATE TABLE IF NOT EXISTS features_stats (
                 data_id SERIAL PRIMARY KEY,
                 dataset_name VARCHAR NOT NULL,
@@ -62,9 +59,9 @@ with DAG(
                 {', '.join([(x) + ' FLOAT' for x in config.sql_mean_columns])},
                 {', '.join([(x) + ' FLOAT' for x in config.sql_std_columns])},
                 {', '.join([(x) + ' FLOAT' for x in config.sql_missing_columns])};
-              """,
+        """,
     )
 
     end_task = EmptyOperator(task_id="end")
 
-    start_task >> create_main_stats_table >> create_feature_stat_table >> end_task
+    start_task >> [create_main_stats_table, create_feature_stat_table] >> end_task
