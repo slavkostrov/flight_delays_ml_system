9e6b508521fc01bf9cf8790fe9ce8f644e861a87
Slava <slavkostrov@gmail.com>
2022-10-30 15:39:47 +0300

    [ADD] Added initital SQL tables creation

--- dags/features_monitoring.py
+++ dags/features_monitoring.py
@@ -12,6 +12,7 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator as EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from base_config import Config
 from tasks.prepare_features import collect_raw_data, clean_data, concat_features
@@ -37,6 +38,33 @@ with DAG(
     dag.doc_md = __doc__
 
     start_task = EmptyOperator(task_id="start")
+
+    create_main_stats_table = PostgresOperator(
+        task_id="create_main_stats_table",
+        sql="""
+                CREATE TABLE IF NOT EXISTS main_data_stats (
+                data_id SERIAL PRIMARY KEY,
+                date TIMESTAMP,
+                dataset_name VARCHAR NOT NULL,
+                row_count VARCHAR NOT NULL,
+                column_count DATE NOT NULL,
+                OWNER VARCHAR NOT NULL);
+              """,
+    )
+
+    create_feature_stat_table = PostgresOperator(
+        task_id="create_feature_stat_table",
+        sql="""
+                CREATE TABLE IF NOT EXISTS features_stats (
+                data_id SERIAL PRIMARY KEY,
+                dataset_name VARCHAR NOT NULL,
+                date TIMESTAMP,
+                {', '.join([(x) + ' FLOAT' for x in config.sql_mean_columns)]},
+                {', '.join([(x) + ' FLOAT' for x in config.sql_std_columns)]},
+                {', '.join([(x) + ' FLOAT' for x in config.sql_missing_columns)]};
+              """,
+    )
+
     end_task = EmptyOperator(task_id="end")
 
-    start_task >> end_task
+    start_task >> create_main_stats_table >> create_feature_stat_table >> end_task
